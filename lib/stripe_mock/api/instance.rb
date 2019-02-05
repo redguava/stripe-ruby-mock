1944f1020d6ddc112cda154539d055f39de0b337
Zac Clay <zacclay@gmail.com>
2019-02-05 10:01:11 -0500

    Make it work with new Stripe gem

--- lib/stripe_mock/api/instance.rb
+++ lib/stripe_mock/api/instance.rb
@@ -2,24 +2,34 @@ module StripeMock
 
   @state = 'ready'
   @instance = nil
-  @original_request_method = Stripe.method(:request)
+  @original_execute_request_method = Stripe::StripeClient.instance_method(:execute_request)
 
   def self.start
     return false if @state == 'live'
-    @instance = Instance.new
-    alias_stripe_method :request, @instance.method(:mock_request)
+    @instance = instance = Instance.new
+    Stripe::StripeClient.send(:define_method, :execute_request) { |*args| instance.mock_request(*args) }
     @state = 'local'
   end
 
   def self.stop
     return unless @state == 'local'
-    alias_stripe_method :request, @original_request_method
+    restore_stripe_execute_request_method
     @instance = nil
     @state = 'ready'
   end
 
-  def self.alias_stripe_method(new_name, method_object)
-    Stripe.define_singleton_method(new_name) {|*args| method_object.call(*args) }
+  # Yield the given block between StripeMock.start and StripeMock.stop
+  def self.mock(&block)
+    begin
+      self.start
+      yield
+    ensure
+      self.stop
+    end
+  end
+
+  def self.restore_stripe_execute_request_method
+    Stripe::StripeClient.send(:define_method, :execute_request, @original_execute_request_method)
   end
 
   def self.instance; @instance; end
